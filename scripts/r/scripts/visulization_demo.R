b269e849a5b0809c7cd6e22014eb82eca1f8b999
Bill Huang <billhuang@ip-106-159.wireless.oberlin.edu>
2016-08-24 10:21:20 -0400

    Rewrite visualization_demo.R for two or higher dimensional data visualization

--- scripts/r/scripts/visulization_demo.R
+++ scripts/r/scripts/visulization_demo.R
@@ -1,75 +1,110 @@
 library(mixtools)
 library(MASS)
 library(segmented)
+library(reshape)
 
-#Function
-PCA_Visualization <- function(
-    data_path,
-    results_path,
-    obs.filename = "obs.txt",
-    z.filename = "z.txt",
-    h.filename = "h.txt",
-    w.filename = "W/W.txt",
-    thetastar.dirname = "thetastar"
-)
-{
-    print("Reading in observations...")
-    obs <- read.table(paste(data_path, obs.filename, sep = "/"))
-    print("Reading state labels...")
-    z <- read.table(paste(results_path, z.filename, sep = "/"), skip = 1)
-    print("Reading noise variances...")
-    H <- read.table(paste(results_path, h.filename, sep = "/"), skip = 1)
-    print("Reading emission weights...")
-    W <- read.table(paste(results_path, w.filename, sep = "/"))
-    print("Reading latent state vectors...")
-    theta_list <- list.files(path=paste(results_path, thetastar.dirname, sep = "/"),
-                             pattern="*txt", full.names=TRUE)
-    ## get basic info and transform W and H into matrix for matrix computation
-    n_dimension <- ncol(obs)
-    n_observation <- nrow(obs)
-    n_iteration <- nrow(z)
-    w <- as.matrix(W)
-    h <- as.matrix(H[,2:ncol(H)])
-    ## Two cases: higher dimension or two dimension
-    if (n_dimension != 2)
-    {
-        print("Data is not two dimensional, we use PCA.")
-        pca_obs <- prcomp(obs, scale.=TRUE)
-        scores_obs <- as.data.frame(pca_obs$x)
-        transform <- cbind(as.vector(pca_obs$rotation[,1]),as.vector(pca_obs$rotation[,2]))
-        pdf()
-        for (i in 1:n_iteration){
-            thetastar <- read.table(theta_list[i])
-            theta_star <- as.matrix(cbind(thetastar,1))
-            sigma <- matrix(0,n_dimension,n_dimension)
-            for (k in 1:n_dimension){sigma[k,k]=sqrt(1./h[i,k])}
-            new_sigma <- t(transform) %*% sigma %*% transform
-            mean <- theta_star %*% w
-            classification=t(z[i,3:ncol(z)])
-            plot(PC1~PC2,data=scores_obs,col=classification,pch=16,cex=.8)
-            for (j in 1:n_observation){
-                new_mean <- as.vector(mean[j,]) %*% transform
-                contour <- ellipse(as.vector(new_mean),new_sigma)
-                lines(contour,col=classification[j])
-            }
-        }
-        dev.off()
-    } else{
-        print("Data is two dimensional.")
-        pdf()
-        for (i in 1:n_iteration){
-            thetastar <- read.table(theta_list[i])
-            theta_star <- as.matrix(cbind(thetastar,1))
-            sigma <- matrix(0,n_dimension,n_dimension)
-            for (k in 1:n_dimension){sigma[k,k]=sqrt(1./h[i,k])}
-            mean <- theta_star %*% w
-            classification=t(z[i,3:ncol(z)])
-            plot(V1~V2,data=obs,col=classification,pch=16,cex=.8)
-            for (j in 1:n_observation){
-                contour <- ellipse(as.vector(mean[j,]),sigma)
-                lines(contour,col=classification[j])
-            }
-        }
-        dev.off()
+Ellipse_Plot <- function(obs, state, mean, cov, iteration_nums){
+    plot(y~x, data=obs, col=state, pch=16, cex=.8, xlab=" ", ylab=" ",
+    main=toString(iteration_nums))
+    for (j in 1:nrow(obs)){
+        contour <- ellipse(as.vector(mean[j,]), cov)
+        lines(contour, col=state[j])
     }
 }
+
+relabel <- function(old_state){
+    len <- length(old_state)
+    unique_element <- unique(old_state)
+    num_of_unique <- length(unique_element)
+    new_state <- rep(0, len)
+    for (k in 1:len){
+        for (l in 1:num_of_unique){
+            if(old_state[k] == unique_element[l]){
+                new_state[k] = l
+                break
+            }
+        }
+    }
+    return (new_state)
+}
+
+
+Visualization <- function(obs, z, h, mean_files, W, thetastar_files, output_path){
+    dimensions <- ncol(obs)
+    n_obs <- nrow(obs)
+    iterations <- nrow(z)
+    h <- as.matrix(h[,-1])
+    W <- as.matrix(W)
+    iteration_num <- as.vector(z[,1])
+    z <- as.matrix(z[,-c(1:2)])
+    pdf(output_path)
+    if (dimensions == 2){
+        obs <- rename(obs, c(V1="x", V2="y"))
+        if (identical(mean_files, character(0))){
+            for (i in 1:iterations){
+                theta_star <- read.table(thetastar_files[i])
+                thetastar <- as.matrix(cbind(theta_star,1))
+                mean <- thetastar %*% W
+                cov <- matrix(0, dimensions, dimensions)
+                for (k in 1:dimensions){cov[k,k]=1./h[i,k]}
+                state <- relabel(as.vector(z[i,]))
+                Ellipse_Plot(obs, state, mean, cov, iteration_num[i])
+            }
+        }
+        else{
+            for (i in 1:iterations){
+                mean <- as.matrix(read.table(mean_files[i]))
+                cov <- matrix(0, dimensions, dimensions)
+                for (k in 1:dimensions){cov[k,k]=1./h[i,k]}
+                state <- relabel(as.vector(z[i,]))
+                Ellipse_Plot(obs, state, mean, cov, iteration_num[i])
+            }
+        }
+    }
+    else{
+        pca <- prcomp(obs, scales=TRUE)
+        pca_scores <- as.data.frame(pca$x)
+        transform <- cbind(as.vector(pca$rotation[,1]), as.vector(pca$rotation[,2]))
+        pca_scores <- pca_scores[,1:2]
+        pca_scores <- rename(pca_scores, c(PC1="x", PC2="y"))
+        if (identical(mean_files, character(0))){
+            for (i in 1:iterations){
+                theta_star <- read.table(thetastar_files[i])
+                thetastar <- as.matrix(cbind(theta_star,1))
+                mean <- thetastar %*% W %*% transform
+                ori_cov <- matrix(0, dimensions, dimensions)
+                for (k in 1:dimensions){ori_cov[k,k]=1./h[i,k]}
+                cov <- t(transform) %*% ori_cov %*% transform
+                state <- relabel(as.vector(z[i,]))
+                Ellipse_Plot(pca_scores, state, mean, cov, iteration_num[i])
+            }
+        }
+        else{
+            for (i in 1:iterations){
+                mean <- as.matrix(read.table(mean_files[i])) %*% transform
+                ori_cov <- matrix(0, dimensions, dimensions)
+                for (k in 1:dimensions){ori_cov[k,k]=1./h[i,k]}
+                cov <- t(transform) %*% ori_cov %*% transform
+                state <- relabel(as.vector(z[i,]))
+                Ellipse_Plot(pca_scores, state, mean, cov, iteration_num[i])
+            }
+        }
+    }
+    dev.off()
+}
+
+#import file
+args <- commandArgs(trailingOnly = TRUE)
+data_path <- toString(args[2])
+results_path <- toString(args[1])
+obs <- read.table(paste("data",data_path,"obs.txt",sep="/"))
+z <- read.table(paste("results",results_path,"z.txt",sep="/"), skip=1)
+h <- read.table(paste("results",results_path,"h.txt",sep="/"), skip=1)
+mean_root <- paste("results",results_path,"mean_by_state",sep="/")
+mean_files <- list.files(path=mean_root, pattern="*txt", full.names=TRUE)
+W <- read.table(paste("results",results_path,"W/W.txt",sep="/"))
+thetastar_root <- paste("results",results_path,"thetastar",sep="/")
+thetastar_files <- list.files(path=thetastar_root, pattern="*txt", full.names=TRUE)
+output_path <- paste("results",results_path,"Visualization.pdf",sep="/")
+
+Visualization(obs, z, h, mean_files, W, thetastar_files, output_path)
