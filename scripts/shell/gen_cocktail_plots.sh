f772e7c7217ccbbf0f7c525f47f2a39186606c53
Colin Dawson <cdawson@oberlin.edu>
2017-02-23 17:04:03 -0500

    added shell script directory

--- scripts/shell/gen_cocktail_plots.sh
+++ scripts/shell/gen_cocktail_plots.sh
@@ -5,7 +5,8 @@ SMOOTH=100
 COCKTAIL_RESULTS_PATH="cocktail_s16_m12/hyper_alpha/h10.0_nocs_cp0"
 COCKTAIL_DATA_PATH="cocktail_s16_m12/h10.0_nocs/cp0"
 PROJECT_ROOT="../data"  #assume this will be run from <hamlet_root>/experiment
+RSCRIPT_ROOT="scripts/r/scripts"
 
-Rscript scripts/r/scripts/master_visualization.R -q "cocktail_icml.txt" -d $COCKTAIL_RESULTS_PATH -s $SMOOTH -b $BURNIN -p "." --binary=1 --remove=A -g $COCKTAIL_DATA_PATH
-Rscript master_visualization.R -q "cocktail_icml_noBFact.txt" -d $COCKTAIL_RESULTS_PATH -s $SMOOTH -b $BURNIN -p "." -v alpha,gamma,train_log_likelihood,test_log_likelihood
-Rscript master_visualization.R -q "cocktail_icml_LT_only.txt" -d $COCKTAIL_RESULTS_PATH -s $SMOOTH -b $BURNIN -p "." -v lambda
+Rscript $RSCRIPT_ROOT/master_visualization.R -q "cocktail_icml.txt" -d $COCKTAIL_RESULTS_PATH -s $SMOOTH -b $BURNIN -p "." --binary=1 --remove=A -g $COCKTAIL_DATA_PATH
+Rscript $SCRIPTS_ROOT/master_visualization.R -q "cocktail_icml_noBFact.txt" -d $COCKTAIL_RESULTS_PATH -s $SMOOTH -b $BURNIN -p "." -v alpha,gamma,train_log_likelihood,test_log_likelihood
+Rscript $RSCRIPT_ROOT/master_visualization.R -q "cocktail_icml_LT_only.txt" -d $COCKTAIL_RESULTS_PATH -s $SMOOTH -b $BURNIN -p "." -v lambda
