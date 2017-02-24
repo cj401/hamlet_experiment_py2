d2624c90b67cbc7138ed52d80a71f03ec7b13a35
Colin Dawson <cdawson@oberlin.edu>
2017-02-23 19:17:06 -0500

    updated cocktail plot script

--- scripts/shell/gen_cocktail_plots.sh
+++ scripts/shell/gen_cocktail_plots.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
+## Run on venti from <hamlet_root>/experiment
+
 export BURNIN=1000
+export LOW_BURNIN=100
 export SMOOTH=100
 export COCKTAIL_RESULTS_PATH="cocktail_s16_m12/hyper_alpha/h10.0_nocs_cp0"
 export COCKTAIL_DATA_PATH="cocktail_s16_m12/h10.0_nocs/cp0"
@@ -12,6 +15,6 @@ cd $THIS_DIR/$RSCRIPT_ROOT
 
 Rscript master_visualization.R -q "cocktail_icml.txt" -d $COCKTAIL_RESULTS_PATH -s $SMOOTH -b $BURNIN -p "." --binary=1 --remove=A -g $COCKTAIL_DATA_PATH
 Rscript master_visualization.R -q "cocktail_icml_noBFact.txt" -d $COCKTAIL_RESULTS_PATH -s $SMOOTH -b $BURNIN -p "." -v alpha,gamma,train_log_likelihood,test_log_likelihood
-Rscript master_visualization.R -q "cocktail_icml_LT_only.txt" -d $COCKTAIL_RESULTS_PATH -s $SMOOTH -b $BURNIN -p "." -v lambda
+Rscript master_visualization.R -q "cocktail_icml_LT_only.txt" -d $COCKTAIL_RESULTS_PATH -s $SMOOTH -b $LOW_BURNIN -p "." -v lambda
 
 cd $THIS_DIR
