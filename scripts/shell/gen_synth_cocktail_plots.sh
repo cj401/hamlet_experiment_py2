c148e02f1ed95ad43cd08804d39af9a5172d6ee6
Colin Dawson <cdawson@oberlin.edu>
2017-03-23 13:34:15 -0400

    new queries

--- scripts/shell/gen_synth_cocktail_plots.sh
+++ scripts/shell/gen_synth_cocktail_plots.sh
@@ -14,7 +14,7 @@ export THIS_DIR=$(pwd)
 
 cd $THIS_DIR/$RSCRIPT_ROOT
 
-Rscript master_visualization.R -q "cocktail_icml.txt" -d $COCKTAIL_RESULTS_PATH -s $SMOOTH -b $BURNIN -p "." --binary=1 --remove=A -g $COCKTAIL_DATA_PATH -r $PROJECT_ROOT
+Rscript master_visualization.R -q "synth_cocktail_alpha.txt" -d $COCKTAIL_RESULTS_PATH -s $SMOOTH -b $BURNIN -p "." --binary=1 --remove=A -g $COCKTAIL_DATA_PATH -r $PROJECT_ROOT
 Rscript master_visualization.R -q "cocktail_icml_noBFact.txt" -d $COCKTAIL_RESULTS_PATH -s $SMOOTH -b $BURNIN -p "." -v alpha,gamma,train_log_likelihood,test_log_likelihood,n_dot -r $PROJECT_ROOT
 Rscript master_visualization.R -q "cocktail_icml_LT_only.txt" -d $COCKTAIL_RESULTS_PATH -s $SMOOTH -b $LOW_BURNIN -p "." -v lambda -r $PROJECT_ROOT
 
