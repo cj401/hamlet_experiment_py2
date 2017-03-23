274fa4ba353ce42f7e51d5693079d10686513388
Colin Dawson <cdawson@oberlin.edu>
2017-03-23 13:14:13 -0400

    added new plotting script

--- scripts/shell/gen_synth_plots.sh
+++ scripts/shell/gen_synth_plots.sh
@@ -13,7 +13,7 @@ export THIS_DIR=$(pwd)
 cd $THIS_DIR/$RSCRIPT_ROOT
 
 Rscript master_visualization.R -q "synth16.txt" -d $RESULTS_PATH -s $SMOOTH -b $BURNIN -p "." --binary=1 --remove=A -g $DATA_PATH -r $PROJECT_ROOT
-Rscript master_visualization.R -q "synth16_noBFact.txt" -d $RESULTS_PATH -s $SMOOTH -b $BURNIN -p "." -v alpha,gamma,train_log_likelihood,test_log_likelihood,n_dot,A -r $PROJECT_ROOT
+Rscript master_visualization.R -q "synth16_noBFact.txt" -d $RESULTS_PATH -s $SMOOTH -b $BURNIN -p "." -v alpha,gamma,train_log_likelihood,n_dot,A -r $PROJECT_ROOT
 Rscript master_visualization.R -q "synth16_LT_only.txt" -d $RESULTS_PATH -s $SMOOTH -b $BURNIN -p "." -v lambda -r $PROJECT_ROOT
 
 cd $THIS_DIR
